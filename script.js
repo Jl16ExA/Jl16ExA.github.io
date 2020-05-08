4e67a89838ed583618dc4712f8f9c95b7451626c
Juan Lopez <juandlopezb160@gmail.com>
2020-05-08 13:31:25 -0500

    brak for photos

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 
-
-var Message = document.getElementById("GenderTXT").innerHTML;
-
-
+let Message = document.getElementById("GenderTXT").innerHTML;
 
 function men(){
-     Message = "Nice Style";  
+     document.getElementById("GenderTXT").innerHTML = "Nice Style";  
      document.getElementById("imgMen").style.visibility="visible";
 }
 
 function women(){
-    Message = "Nice Style";
+    document.getElementById("GenderTXT").innerHTML = "Nice Style"; 
     document.getElementById("imgWomen").style.visibility="visible";
 }
    
