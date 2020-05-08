3f4b4bfcda078787e3808e3e4a97b099cead54b6
Juan Lopez <juandlopezb160@gmail.com>
2020-05-08 13:40:28 -0500

    Hide and Show Images

--- script.js
+++ script.js
@@ -2,12 +2,22 @@
 let Message = document.getElementById("GenderTXT").innerHTML;
 
 function men(){
-     document.getElementById("GenderTXT").innerHTML = "Nice Style";  
+     document.getElementById("GenderTXT").innerHTML = "Nice Style Boy!";  
      document.getElementById("imgMen").style.visibility="visible";
 }
 
 function women(){
-    document.getElementById("GenderTXT").innerHTML = "Nice Style"; 
+    document.getElementById("GenderTXT").innerHTML = "Nice Style Girl!"; 
     document.getElementById("imgWomen").style.visibility="visible";
 }
    
+function menquit(){
+    document.getElementById("GenderTXT").innerHTML = "Nice Style Boy!";  
+    document.getElementById("imgMen").style.visibility="hidden";
+}
+
+function womenquit(){
+   document.getElementById("GenderTXT").innerHTML = "Nice Style Girl!"; 
+   document.getElementById("imgWomen").style.visibility="hidden";
+}
+  
