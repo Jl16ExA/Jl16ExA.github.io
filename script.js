0eb05e7c6fd7ebd7a32494047d1a7f59caed8c9b
Juan Lopez <juandlopezb160@gmail.com>
2020-05-09 22:22:45 -0500

    New Organization of The Code

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-
 let Message = document.getElementById("GenderTXT").innerHTML;
 
-function men(){
-     document.getElementById("GenderTXT").innerHTML = "NICE STYLE MAN";  
-     document.getElementById("imgMen").style.visibility="visible";
+function men() {
+  document.getElementById("GenderTXT").innerHTML = "NICE STYLE MAN";
+  document.getElementById("imgMen").style.visibility = "visible";
 }
 
-function women(){
-    document.getElementById("GenderTXT").innerHTML = "NICE STYLE GIRL"; 
-    document.getElementById("imgWomen").style.visibility="visible";
-}
-   
-function menquit(){
-    document.getElementById("imgMen").style.visibility="hidden";
+function women() {
+  document.getElementById("GenderTXT").innerHTML = "NICE STYLE GIRL";
+  document.getElementById("imgWomen").style.visibility = "visible";
 }
 
-function womenquit(){
-   document.getElementById("imgWomen").style.visibility="hidden";
+function menquit() {
+  document.getElementById("imgMen").style.visibility = "hidden";
 }
-  
+
+function womenquit() {
+  document.getElementById("imgWomen").style.visibility = "hidden";
+}
